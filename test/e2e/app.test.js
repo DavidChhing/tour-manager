d5d4d8e16ea58a325d2a15e56b6c225d1667de5c
Davidchhing <davidhchhing@gmail.com>
2018-10-11 12:02:28 -0700

    added util folder, e2e folder, and unit folder

---
+++ test/e2e/app.test.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const { dropCollection } = require('./db');
+const request = require('supertest');
+const app = require('../../lib/app');
+const Chance = require('chance');
+const chance = new Chance();
+
+describe('describe a tour on post', () => {
+
+});
