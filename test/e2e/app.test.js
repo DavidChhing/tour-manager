4c14504843bfa633552db1358e9fc7a96a0522e0
Davidchhing <davidhchhing@gmail.com>
2018-10-16 17:19:25 -0700

    Starting to write app e2e test

--- test/e2e/app.test.js
+++ test/e2e/app.test.js
@@ -1,10 +1,51 @@
 require('dotenv').config();
-const { dropCollection } = require('./db');
+const { dropCollection } = require('../unit/db');
 const request = require('supertest');
 const app = require('../../lib/app');
 const Chance = require('chance');
 const chance = new Chance();
 
-describe('describe a tour on post', () => {
+describe('tours end to end test', () => {
+
+    let tours = [
+        {
+            title: 'Rolly Roos',
+            activities: ['bar hopping', 'playing tag'],
+            launchDate: chance.date()
+        },
+        {
+            title: 'Round n Round',
+            activities: ['Monkey Watching', 'Walnut picking'],
+            launchDate: chance.date()
+        },
+        {
+            title: 'Conduit Run',
+            activities: ['Baking classes', 'Tree climbing'],
+            launchDate: chance.date()
+        },
+    ];    
+
+    let createdTours;
+
+    const createdTour = tour => {
+        return request(app)
+            .post('/api/tours')
+            .send(tour)
+            .then(res => res.body);
+    };
+
+    beforeEach(() => {
+        return dropCollection('tours');
+    });
+
+    beforeEach(() => {
+        return Promise.all(tours.map(createdTour))
+            .then(tourRes => createdTours = tourRes);
+    });
+
+    
+
+
+
 
 });
